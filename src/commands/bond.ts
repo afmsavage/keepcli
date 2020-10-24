e0f3db816b38a0f5947b1bff032c9dc28144d174
Antonio Savage <afmsavage@gmail.com>
2020-10-24 16:45:35 -0500

    config and bond working, removed some packages

--- src/commands/bond.ts
+++ src/commands/bond.ts
@@ -1,5 +1,5 @@
+/* eslint-disable no-console */
 import {Command, flags} from '@oclif/command'
-// const getBond = require('./get-bond.js')
 
 const ethers = require('ethers')
 const KeepBonding = require('@keep-network/keep-ecdsa/artifacts/KeepBonding.json')
@@ -17,27 +17,24 @@ async function getBond(address: any) {
 
     const keepBondingContract = new ethers.Contract(KeepBonding.networks['1'].address, KeepBonding.abi, ip)
     const available = await keepBondingContract.unbondedValue(addr)
-    return `available for bonding: ${ethers.utils.formatEther(available)}`
+    return `ETH available for bonding: ${ethers.utils.formatEther(available)} in wallet ${addr}`
   } catch (error) {
     console.error(`Could not authorize: ${error.message}`)
   }
 }
 
 export default class Bond extends Command {
-  static description = 'Check free bond of ECDSA Signing node'
+  static description = 'Check free bond of ECDSA Signing node. Leave address off if you want to check the bond of the configured address'
 
   static examples = [
-    `$ keepcli bond 0x0000000000000000000000000000000000
-eth available for bonding: 20.2345
+    `$ keepcli bond 0x0000000000000000000000000000000000000000
+> eth available for bonding: 20.2345
 `,
   ]
 
     static flags = {
       help: flags.help({char: 'h'}),
       // flag with a value (-a, --addr=VALUE)
-      addr: flags.string({char: 'a', description: 'operator address to check'}),
-      // flag with no value (-f, --force)
-      force: flags.boolean({char: 'f'}),
     }
 
   static args = [{
@@ -47,14 +44,10 @@ eth available for bonding: 20.2345
   }]
 
   async run() {
-    const {args, flags} = this.parse(Bond)
-    // INFO: need to figure out this part still - loading the config
-    const config = require(path.join(this.config.configDir, 'config.json'))
-    const addr = flags.addr ?? 'world'
-    this.log(await getBond(addr)
-    )
-    if (args.name) {
-      this.log(`please ${name} enter an address`)
-    }
+    const {args} = this.parse(Bond)
+
+    const cliConfig = require(path.join(this.config.configDir, 'config.json'))
+    const addr = args.address ?? cliConfig.address
+    this.log(await getBond(addr))
   }
 }
--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,35 +1,31 @@
 import {Command, flags} from '@oclif/command'
 const fs = require('fs')
-import * as fsx from 'fs-extra'
 import * as path from 'path'
-import cli from 'cli-ux'
 
 export default class Config extends Command {
-  static description = 'configure keepcli with your node\'s operator address'
+  static description = 'Configure keepcli with your node\'s operator address.'
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{
+    name: 'address',
+    required: true,
+    description: 'operator address to configure CLI with',
+  }]
 
   async run() {
     fs.mkdir(this.config.configDir, {recursive: true}, (err: any) => {
       if (err) throw err
     })
 
-    const {args, flags} = this.parse(Config)
+    const {args} = this.parse(Config)
     const configFile = await path.join(this.config.configDir, 'config.json')
-    const opAddr = await cli.prompt('What is your address?')
-
-    this.log(`please find your config file at ${configFile}`)
-    fs.writeFile(configFile, JSON.stringify({address: `${opAddr}`}), function (err: any) {
-      // eslint-disable-next-line no-console
-      if (err) return console.log(`Sorry, couldn't write config.\nError: ${err}`)
+    this.log(`CLI is now configured with address: ${args.address}`)
+    this.log(`Your config file is located at ${configFile}`)
+    fs.writeFile(configFile, JSON.stringify({address: `${args.address}`}),  (err: any) => {
+      if (err) return this.log(`Sorry, couldn't write config.\nError: ${err}`)
     })
   }
 }
